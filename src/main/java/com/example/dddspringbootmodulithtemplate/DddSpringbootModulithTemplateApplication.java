a29fa5e2ae3d9e2fde5d26a621e349f9803cb87d
overGDev <joaquinorozco2004@gmail.com>
2026-07-20 23:08:02 -0500

    chore: initial commit

---
+++ src/main/java/com/example/dddspringbootmodulithtemplate/DddSpringbootModulithTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddspringbootmodulithtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddSpringbootModulithTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddSpringbootModulithTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddspringbootmodulithtemplate/DddSpringbootModulithTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddspringbootmodulithtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddSpringbootModulithTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
